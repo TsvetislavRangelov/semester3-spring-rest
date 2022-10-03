d0658497afaed4d2aa9fa8666ff3e03bcf17731a
Tsvetislav Rangelov <cveti2002mnt@gmail.com>
2022-10-03 21:46:20 +0200

    tweaks to entity classes

--- ReReddit/src/main/java/sem3/its/ReReddit/persistence/UserRepository.java
+++ ReReddit/src/main/java/sem3/its/ReReddit/persistence/UserRepository.java
@@ -8,5 +8,6 @@ import sem3.its.ReReddit.persistence.entity.UserEntity;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 
 }
